794f1987d1591e29092957bf5d8b66b007dcd38d
a0979 <s1081707@gm.pu.edu.tw>
2021-06-25 13:16:17 +0800

    bug已修正 此為報告版本

--- app/src/main/java/tw/edu/pu/s1081707/baby1/AniMainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1081707/baby1/AniMainActivity.kt
@@ -1,99 +1,145 @@
 package tw.edu.pu.s1081707.baby1
 
-import android.media.AudioManager
-import android.media.SoundPool
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_car_main.*
 
 class AniMainActivity : AppCompatActivity() {
+    lateinit var mper: MediaPlayer
 
-    private var soundPool1: SoundPool? = null
-    private val soundId = 1
-
-    private var soundPool2: SoundPool? = null
-
-    private var soundPool3: SoundPool? = null
-
-    private var soundPool4: SoundPool? = null
-
-    private var soundPool5: SoundPool? = null
-
-    private var soundPool6: SoundPool? = null
-
-    private var soundPool7: SoundPool? = null
-
-    private var soundPool8: SoundPool? = null
+//    private var soundPool1: SoundPool? = null
+//    private val soundId = 1
+//
+//    private var soundPool2: SoundPool? = null
+//
+//    private var soundPool3: SoundPool? = null
+//
+//    private var soundPool4: SoundPool? = null
+//
+//    private var soundPool5: SoundPool? = null
+//
+//    private var soundPool6: SoundPool? = null
+//
+//    private var soundPool7: SoundPool? = null
+//
+//    private var soundPool8: SoundPool? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ani_main)
 
-        soundPool1 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        soundPool1!!.load(baseContext, R.raw.animc1, 0)
+        mper = MediaPlayer()
 
-        soundPool2 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        soundPool2!!.load(baseContext, R.raw.animc2, 1)
-
-        soundPool3 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        soundPool3!!.load(baseContext, R.raw.animc3, 3)
-
-        soundPool4 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        soundPool4!!.load(baseContext, R.raw.animc4, 4)
-
-        soundPool5 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        soundPool5!!.load(baseContext, R.raw.animc5, 5)
-
-        soundPool6 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        soundPool6!!.load(baseContext, R.raw.animc6, 6)
-
-        soundPool7 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        soundPool7!!.load(baseContext, R.raw.animc7, 7)
-
-        soundPool8 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
-        soundPool8!!.load(baseContext, R.raw.animc8, 8)
+//        soundPool1 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+//        soundPool1!!.load(baseContext, R.raw.animc1, 0)
+//
+//        soundPool2 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+//        soundPool2!!.load(baseContext, R.raw.animc2, 1)
+//
+//        soundPool3 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+//        soundPool3!!.load(baseContext, R.raw.animc3, 3)
+//
+//        soundPool4 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+//        soundPool4!!.load(baseContext, R.raw.animc4, 4)
+//
+//        soundPool5 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+//        soundPool5!!.load(baseContext, R.raw.animc5, 5)
+//
+//        soundPool6 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+//        soundPool6!!.load(baseContext, R.raw.animc6, 6)
+//
+//        soundPool7 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+//        soundPool7!!.load(baseContext, R.raw.animc7, 7)
+//
+//        soundPool8 = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+//        soundPool8!!.load(baseContext, R.raw.animc8, 8)
 
     }
 
-    fun playSound1(view: View) {
-        soundPool1?.play(soundId, 1F, 1F, 0, 0, 1F)
-        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
-    }
+//    fun playSound1(view: View) {
+//        soundPool1?.play(soundId, 1F, 1F, 0, 0, 1F)
+//        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    fun playSound2(view: View) {
+//        soundPool2?.play(soundId, 1F, 1F, 1, 0, 1F)
+//        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    fun playSound3(view: View) {
+//        soundPool3?.play(soundId, 1F, 1F, 2, 0, 1F)
+//        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    fun playSound4(view: View) {
+//        soundPool4?.play(soundId, 1F, 1F, 3, 0, 1F)
+//        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    fun playSound5(view: View) {
+//        soundPool5?.play(soundId, 1F, 1F, 4, 0, 1F)
+//        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    fun playSound6(view: View) {
+//        soundPool6?.play(soundId, 1F, 1F, 5, 0, 1F)
+//        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    fun playSound7(view: View) {
+//        soundPool7?.play(soundId, 1F, 1F, 6, 0, 1F)
+//        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    fun playSound8(view: View) {
+//        soundPool8?.play(soundId, 1F, 1F, 7, 0, 1F)
+//        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+//    }
 
-    fun playSound2(view: View) {
-        soundPool2?.play(soundId, 1F, 1F, 1, 0, 1F)
-        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
-    }
+    fun StartPlay(v:View){
+        mper.reset()
+        when (v.id) {
+            R.id.ani1_btn -> {
+                mper = MediaPlayer.create(this, R.raw.animc1)
+                mper.start()
+            }
 
-    fun playSound3(view: View) {
-        soundPool3?.play(soundId, 1F, 1F, 2, 0, 1F)
-        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
-    }
+            R.id.ani2_btn -> {
+                mper = MediaPlayer.create(this, R.raw.animc2)
+                mper.start()
+            }
 
-    fun playSound4(view: View) {
-        soundPool4?.play(soundId, 1F, 1F, 3, 0, 1F)
-        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
-    }
+            R.id.ani3_btn -> {
+                mper = MediaPlayer.create(this, R.raw.animc3)
+                mper.start()
+            }
 
-    fun playSound5(view: View) {
-        soundPool5?.play(soundId, 1F, 1F, 4, 0, 1F)
-        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
-    }
+            R.id.ani4_btn -> {
+                mper = MediaPlayer.create(this, R.raw.animc4)
+                mper.start()
+            }
 
-    fun playSound6(view: View) {
-        soundPool6?.play(soundId, 1F, 1F, 5, 0, 1F)
-        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
-    }
+            R.id.ani5_btn -> {
+                mper = MediaPlayer.create(this, R.raw.animc5)
+                mper.start()
+            }
 
-    fun playSound7(view: View) {
-        soundPool7?.play(soundId, 1F, 1F, 6, 0, 1F)
-        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
-    }
+            R.id.ani6_btn -> {
+                mper = MediaPlayer.create(this, R.raw.animc6)
+                mper.start()
+            }
 
-    fun playSound8(view: View) {
-        soundPool8?.play(soundId, 1F, 1F, 7, 0, 1F)
-        Toast.makeText(this, "Playing sound. . . .", Toast.LENGTH_SHORT).show()
+            R.id.ani7_btn -> {
+                mper = MediaPlayer.create(this, R.raw.animc7)
+                mper.start()
+            }
+
+            R.id.ani8_btn -> {
+                mper = MediaPlayer.create(this, R.raw.animc8)
+                mper.start()
+            }
+        }
     }
 }
